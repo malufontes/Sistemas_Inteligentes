5413445df9e299f738f026a079be98d9962a10cb
Malu Fontes <malufontes00@gmail.com>
2022-05-24 14:18:48 -0300

    pelo menos se mexe kkkk

--- trabalho_1/maze_solver.py
+++ trabalho_1/maze_solver.py
@@ -1,5 +1,6 @@
 from ipaddress import collapse_addresses
 from matplotlib.pyplot import grid
+from numpy import angle
 
 
 row1 = [2,0,0,0,0,0,0,0,0,0]
@@ -42,7 +43,7 @@ for row in range(0,nrows):
             inicial_row = row
             inicial_col = col
 
-# print(inicial_row,inicial_col)
+print(inicial_row,inicial_col)
 
 class Cell:
     def __init__(self,row,col,id):
@@ -92,13 +93,54 @@ for x in range(0,nrows):
 print(grid_cells[0][0].id)
 
         
-class Agente:
+class AgenteBFS:
+    def __init__(self,row,col):
+        self.row, self.col = row, col
+        self.lista = []
+        self.lista_checkpoint = []
+        self.sucess = False
+
+    def expandir_no(self):
+        row = self.row
+        col = self.col
+        self.teste_de_objetivo(row,col)
+        print("BFS se movimentando")
+        if(row > 0 and not self.sucess):
+            if(grid_cells[row-1][col].id ==1):
+                self.teste_de_objetivo(row-1,col)
+                # self.expandir_no()
+        if(row<(nrows-1) and not self.sucess):
+            if(grid_cells[row+1][col].id ==1):
+                self.teste_de_objetivo(row+1,col)
+                # self.expandir_no()
+        if(col>0 and not self.sucess):
+            if(grid_cells[row][col-1].id ==1):
+                self.teste_de_objetivo(row,col-1)
+                # self.expandir_no()
+        if(col<(nrows-1) and not self.sucess):
+            if(grid_cells[row][col+1].id ==1):
+                self.teste_de_objetivo(row,col+1)
+                # self.expandir_no()
+
+
+    def teste_de_objetivo(self, row, col):
+        self.row = row
+        self.col = col
+        if(grid_cells[self.row-1][self.col].id ==3):
+            self.sucess = False
+
+    def draw(self):
+        pygame.draw.rect(screen, pygame.Color('yellow'), pygame.Rect((TILE*self.row),(TILE*self.col),TILE,TILE))
+
+
+class AgenteDFS:
     def __init__(self,x,y):
         self.x, self.y = x, y
 
     def move(self):
-        print("se movimentando")
+        print("DFS se movimentando")
 
+agente = AgenteBFS(inicial_row,inicial_col)
 
 import pygame  
   
@@ -118,6 +160,9 @@ while not done:
     for row in range(0,nrows):
         for col in range(0,ncolumns):
             grid_cells[row][col].draw()
+
+    agente.draw()
+    agente.expandir_no()
 
     
     pygame.display.flip()  
