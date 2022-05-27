e0bd4eb2f7d3e60bf9f35b0c22c38e387cc962e2
Malu Fontes <malufontes00@gmail.com>
2022-05-27 11:29:42 -0300

    DFS funcionando mas sem final

--- trabalho_1/maze_solver.py
+++ trabalho_1/maze_solver.py
@@ -247,13 +247,131 @@ class AgenteBFS:
 
 
 class AgenteDFS:
-    def __init__(self,x,y):
-        self.x, self.y = x, y
+    def __init__(self,row,col):
+        self.row, self.col = row, col
+        self.last_row, self.last_col = row, col
+        self.lista = []
+        self.lista_checkpoint = []
+        self.sucess = False
 
-    def move(self):
+    def expandir_no(self):
+        
+        row = self.row
+        col = self.col
+        ##self.teste_de_objetivo(row,col)
         print("DFS se movimentando")
 
-agenteBFS = AgenteBFS(inicial_row,inicial_col)
+        self.testa_lados()
+        
+
+    def testa_lados(self): 
+        embaralhador=  random.randint(0,3) #gera um numero aleatorio
+        contador=4                         # contador ate 4 para testar 4 lados 
+
+        #print ("Numero da vez:", embaralhador)  
+        while(contador>0):                  #while testa os 4 lados e nao permite voltar 
+            if embaralhador == 0:
+                if(self.teste_direita()):
+                    return
+            if embaralhador == 1:
+                if(self.teste_esquerda()):
+                    return
+            if embaralhador == 2:
+                if(self.teste_cima()):
+                    return
+            if embaralhador == 3:
+                if(self.teste_baixo()):
+                    return
+            
+            contador = contador -1
+            embaralhador = (embaralhador+1) % 4 #encrementa o embaralhador e zera caso passe de 3
+            
+        ##------------------  caso estaja em um beco sem saida sera necessario voltar, entao apos testar os 4 lados sem sucesso a funcao permite voltar
+        contador = 4
+        while(contador>0):
+            if embaralhador == 0:
+                if(self.teste_direita(True)):
+                    return
+            if embaralhador == 1:
+                if(self.teste_esquerda(True)):
+                    return
+            if embaralhador == 2:
+                if(self.teste_cima(True)):
+                    return
+            if embaralhador == 3:
+                if(self.teste_baixo(True)):
+                    return
+
+            contador = contador -1
+            embaralhador = (embaralhador+1) % 4
+            
+            
+    
+                 
+    def teste_direita(self, voltapermitida = False):
+        print("Volta permitida direita", voltapermitida)     
+        if(self.row<(nrows-1) and not self.sucess): ## se a linha nao for maior que o o numero maximo menos 1 pra nao acessar area inexistente da matriz 
+                if(grid_cells[self.row+1][self.col].id ==1):
+                    if(voltapermitida or self.row+1 != self.last_row ): ## se a volta for permitida ou se nao estiver voltando
+                        print("DIREITA")
+                        print("linha", self.row, "colula :", self.col , "row + 1:", self.row+1 ,  "self last row ", self.last_row)
+                        self.teste_de_objetivo(self.row+1,self.col)
+                        #self.expandir_no()
+                        return(True)
+        return(False)
+                    
+
+    def teste_baixo(self,voltapermitida = False):
+        if(self.col<(nrows-1) and not self.sucess): ## coluna menor que o num maximo menos 1 numero de rows eh igual ao numero de cols
+            if(grid_cells[self.row][self.col+1].id ==1):
+                if(voltapermitida or self.col+1 != self.last_col):
+                    print("BAIXO")
+                    print("linha", self.row, "colula :", self.col ,"col + 1:", self.col+1 , "self last col ", self.last_col)
+                    self.teste_de_objetivo(self.row,self.col+1)
+                    #self.expandir_no()
+                    return(True)
+        return(False)
+                
+    def teste_esquerda(self, voltapermitida = False):
+        if(self.row > 0 and not self.sucess): ## se a linha nao for menor que zero ou seja se nao tenar acessar algo que nao existe
+            if(grid_cells[self.row-1][self.col].id ==1):
+                if(voltapermitida or self.row-1 != self.last_row):
+                    print("ESQUERDA")
+                    print("linha", self.row, "colula :", self.col ,"row - 1:", self.row-1 , "self last row ", self.last_row)
+                    self.teste_de_objetivo(self.row-1,self.col)
+                    #self.expandir_no()
+                    return(True)
+        return(False)
+
+    def teste_cima(self,voltapermitida = False):
+        if(self.col>0 and not self.sucess):  ## coluna maior que zero 
+            if(grid_cells[self.row][self.col-1].id ==1 ):
+                if(voltapermitida or  self.col-1 != self.last_col):
+                    print("CIMA")
+                    print("linha", self.row, "colula :", self.col ,"col - 1:", self.col-1 , "self last col ", self.last_col)
+                    self.teste_de_objetivo(self.row,self.col-1)
+                    #self.expandir_no()
+                    return(True) 
+        return(False)
+
+    def teste_de_objetivo(self, row, col):
+        self.last_col = self.col
+        self.last_row = self.row
+        self.row = row
+        self.col = col
+        if(grid_cells[self.row-1][self.col].id ==3):
+            self.sucess = True
+
+    def draw(self):
+        pygame.draw.rect(screen, pygame.Color('yellow'), pygame.Rect((TILE*self.row),(TILE*self.col),TILE,TILE))
+
+## ------------------------- Execução 
+modo = input("Selecione BFS[1] ou DFS[2]")        
+
+if modo == "1":
+    agente = AgenteBFS(inicial_row,inicial_col)
+if modo == "2":
+    agente = AgenteDFS(inicial_row,inicial_col)
 
 import pygame  
   
@@ -274,12 +392,12 @@ while not done:
         for col in range(0,ncolumns):
             grid_cells[row][col].draw()
 
-    agenteBFS.draw()
+    agente.draw()
 
-    agenteBFS.visitar_no()
-
-    
-
+    if modo == "1":
+        agente.visitar_no()
+    if modo == "2":
+        agente.expandir_no()
     
     pygame.display.flip()  
     clock.tick(5)
