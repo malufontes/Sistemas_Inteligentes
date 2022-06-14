d8ac3b6cfd8248668603a9b8b096a359e9a4c8b4
Malu Fontes <malufontes00@gmail.com>
2022-06-14 11:53:40 -0300

    consegui mandar uma msg uhuuu

--- trabalho_2/comunicacao_agentes.py
+++ trabalho_2/comunicacao_agentes.py
@@ -1,4 +1,5 @@
 import time
+from turtle import delay
 from spade.agent import Agent
 from spade.behaviour import OneShotBehaviour
 from spade.behaviour import CyclicBehaviour
@@ -8,20 +9,20 @@ from spade.template import Template
 class Sender(Agent):
     class enviar_msg(CyclicBehaviour):
         async def run(self):
-            print("run -> enviar_msg")
+            # print("run -> enviar_msg")
 
-            msg = Message(to="andre@jix.im")     # Instantiate the message
+            msg = Message(to="maluf@jix.im")     # Instantiate the message
             msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
             msg.body = "Oi sumida rs"                    # Set the message content
 
             await self.send(msg)
-            print("mensagem enviada!")
+            # print("mensagem enviada!")
 
         async def on_end(self):
             await self.agent.stop()
 
     async def setup(self):
-        print("Sender inicializado")
+        print("******** Sender inicializado")
         b = self.enviar_msg()
         self.add_behaviour(b)
 
@@ -30,7 +31,7 @@ class Receiver(Agent):
         async def run(self):
             print("run -> receber_msg")
 
-            msg = await self.receive(timeout = 10)
+            msg = await self.receive(timeout = 20)
             if (msg):
                 print("msg recebida: " + msg.body)
             else:
@@ -40,23 +41,26 @@ class Receiver(Agent):
             await self.agent.stop()
 
     async def setup(self):
-        print("Receiver inicializado")
+        print("****** Receiver inicializado")
         b = self.receber_msg()
         template = Template()
         template.set_metadata("performative", "inform")
         self.add_behaviour(b, template)
 
-senderAgent = Sender("andre@jix.im", "RelouSI")
 receiverAgent = Receiver("maluf@jix.im", "RelouSI")
-
-future = senderAgent.start()
-future.result()
 receiverAgent.start()
+future = receiverAgent.start()
+future.result()
+
+senderAgent = Sender("andre@jix.im", "RelouSI")
+senderAgent.start()
+
 
 while receiverAgent.is_alive():
     try:
         time.sleep(1)
     except KeyboardInterrupt:
+        print("Keyboard Interrupt")
         senderAgent.stop()
         receiverAgent.stop()
 print("Agentes assassinados! ;)")
